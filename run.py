9a88ac5d76ca7460d4b97a56c093f01b2c4f56f9
graham-25 <grbills29@googlemail.com>
2024-04-28 14:39:23 +0000

    read data from a txt file

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Hello World")
+
+"""
+This function reads data from a file outside the python file but inside our projrct.
+We put a list of the days of the week.
+"""
+def check_data():
+    file_input = open("days.txt", 'r')
+    data = file_input.read()
+    print(data)
+
+
+"""
+This function reads from a different txt file in our project, and we have asked it only
+to read one line from the text.
+"""
+def read_lines():
+    file_read = open("read.txt", 'r')
+    info = file_read.readline()
+    print(info)
+
+"""
+This function only reads the file up till 20 bytes & then stops.
+"""
+def read_20lines():
+    file_read_20 = open("read.txt", 'r')
+    sale = file_read_20.readlines(20)
+    print(sale)
+
+
+
+check_data()
+#read_lines()
+read_20lines()
---
+++ text.py
@@ -0,0 +1,5 @@
+def write_file():
+    file_input = open("LovePython.txt", 'w')
+    file_input.write("Python is Amazing.")
+
+write_file()
